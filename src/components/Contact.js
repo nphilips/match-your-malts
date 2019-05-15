a5118b755b09e495fb3bbad5983b41d5438c2f80
nphilips <nicholasphilips622@gmail.com>
2019-05-14 20:20:40 -0600

    contacts udate

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="contact">
-            Contact
+            Contact me here!
+            Linkedin: <a target="_blank" href="https://www.linkedin.com/in/nick-philips/">Click Here!</a>
+            GitHub: <a target='_blank' href="https://github.com/nphilips">Click Here!</a>
         </div>
     )
 }
